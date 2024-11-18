e79419f8ed9d19ec399aee14a636ae40dba2cfa6
juhyunkil <wngus0331@gmail.com>
2024-11-18 11:10:04 +0900

    move instance-metric-collector to management-controller

--- Instance-Metric-Collector/4.debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-while [ -z $PODNAME ]
-do
-    PODNAME=`kubectl get po -o=name -A --field-selector=status.phase=Running | grep instance-metric-collector`
-    PODNAME="${PODNAME:4}"
-done
-
-kubectl logs $PODNAME -n system-operator -f 
--- Instance-Metric-Collector/protobuild.sh
+++
@@ -1,3 +0,0 @@
-protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=paths=source_relative    pkg/api/metric/metric.proto -I=.
-protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=paths=source_relative    pkg/api/config/config.proto -I=.
-#protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=paths=source_relative    pkg/api/api/api.proto -I=.
